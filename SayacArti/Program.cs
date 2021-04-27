76ef5841d8ebf01510ec3ed96c9d278b3d861c1d
snmOz <rinicocrini@gmail.com>
2021-04-27 21:02:44 +0300

    Add project files.

---
+++ SayacArti/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SayacArti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("string gir");
+            string gelen = Console.ReadLine();
+            Console.WriteLine("Sayaç kaç haneli olsun.. 5'e kadar olan bir sayı girin");
+            int donus = Convert.ToInt32(Console.ReadLine());
+
+            StringSayac stringSayac = new StringSayac();
+            stringSayac.Sayac(gelen, donus);
+        }
+    }
+}
---
+++ SayacArti/StringSayac.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SayacArti
+{
+    public class StringSayac
+    {
+
+
+        public void Sayac(string gelen, int donus)
+        {
+            Console.Clear();
+
+            int uzunluk = gelen.Length;
+            Console.WriteLine();
+
+
+            if (donus == 2)
+            {
+                for (int i = uzunluk; i < 10; i++)
+                {
+                    if (donus < 10)
+                    {
+
+                        Console.WriteLine(i);
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                    if (i == donus)
+                    {
+                        Console.WriteLine(donus);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+                }
+            }
+
+            else if (donus == 3)
+            {
+                for (int i = uzunluk; i < 100; i++)
+                {
+                    if (i < 10)
+                    {
+                        Console.WriteLine("00" + i);
+
+                    }
+                    if (i >= 10 && i < 100)
+                    {
+                        Console.WriteLine("0" + i);
+                    }
+                    System.Threading.Thread.Sleep(1000);
+
+                    if (i == donus)
+                    {
+                        Console.WriteLine(donus);
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.Clear();
+                    }
+                }
+            }
+            else if (donus == 4)
+            {
+                for (int i = uzunluk; i < 1000; i++)
+                {
+                    if (i < 10)
+                    {
+                        Console.WriteLine("000" + i);
+                    }
+
+                    if (i >= 10 && i < 100)
+                    {
+                        Console.WriteLine("00" + i);
+                    }
+
+                    if (i >= 100 && i < 1000)
+                    {
+                        Console.WriteLine("0" + i);
+                    }
+
+                    System.Threading.Thread.Sleep(1000);
+
+                    if (i == donus)
+                    {
+                        Console.WriteLine(donus);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+                }
+            }
+
+            else if (donus == 5)
+            {
+                for (int i = uzunluk; i < 10000; i++)
+                {
+                    if (i < 10)
+                    {
+                        Console.WriteLine("0000" + i);
+
+                    }
+                    if (i >= 10 && i < 100)
+                    {
+                        Console.WriteLine("000" + i);
+                    }
+
+                    if (i >= 100 && i < 1000)
+                    {
+                        Console.WriteLine("00" + i);
+                    }
+
+                    System.Threading.Thread.Sleep(1000);
+                    if (i == donus)
+                    {
+                        Console.WriteLine(donus);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
